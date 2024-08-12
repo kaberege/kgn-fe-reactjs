0ec8647dc54b95f79c8ca493d3fc2d3f7f9f1cba
kaberege <kabgnestor@gmail.com>
2024-08-12 09:53:16 +0200

    added blue color

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MainContent() {
     return (
-        <main style={{ border: "4px solid #bc3", padding: "5px", margin: "5px", backgroundColor: "rgba(120,220,120,1" }}>
-            <p style={{fontSize: "25px", color: "#974", margin: "3px"}}>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{ border: "4px solid blue", padding: "5px", margin: "5px", backgroundColor: "rgba(120,220,120,1" }}>
+            <p style={{fontSize: "25px", color: "#974", margin: "10px"}}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const UserProfile = (props) => {
   console.log(props);
   return (
-    <div style={{ border: "2px solid red", padding: "5px", margin: "5px", backgroundColor: "rgba(220,220,220,1" }}>
+    <div style={{ border: "2px solid blue", padding: "5px", margin: "10px", backgroundColor: "rgba(220,220,220,1" }}>
       <h2 style={{ color: "#5f3bab", fontWeight: "bold", fontSize: "30px", margin: "4px" }}>{props.name}</h2>
       <p>Age: <span style={{ color: "#003bae", fontSize: "20px", margin: "4px" }}>{props.age}</span></p>
       <p >Bio: <span style={{ color: "#bb3bfb", fontSize: "20px", margin: "4px" }}>{props.bio}</span></p>
