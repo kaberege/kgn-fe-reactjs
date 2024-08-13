076afb33916172cfeaa5335858e7a5dc407f034f
kaberege <kabgnestor@gmail.com>
2024-08-13 09:17:51 +0200

    ne styling to the navBar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const styles = { color: "#3b5", padding: "10px", textDecoration: 'none' }
+    const styles = { color: "#80f", padding: "10px", textDecoration: 'none', fontSize: "30px" }
     return (
-        <div style={{ paddingTop: "30px" }}>
+        <div className="nav-link"
+            style={{
+                padding: "0 30px",
+                backgroundColor: "rgba(80,80,80,0.3)",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
             <Link to="/" style={styles}>Home</Link>
             <Link to="/about" style={styles}>About</Link>
             <Link to="/services" style={styles}>Services</Link>
