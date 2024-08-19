a5185a3776bde71f314a9fd43d58f687f94cdcc6
kaberege <kabgnestor@gmail.com>
2024-08-19 10:06:04 +0200

    alx-react-props updated

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ProfilePage from './components/ProfilePage';
+
+import used from "./components/UserContext/UserContext";
 import UserContext from "./components/UserContext";
 import UserProfile from "./components/UserProfile";
 
 function App() {
   const date = new Date().getFullYear();
   const userData = { name: "Jane Doe", email: "jane.doe@example.com", year: `${date}`, bio: "Professional Dev." };
-
+  console.log(used);
   return (
     <UserContext.Provider value={userData}>
       <ProfilePage />
-      <UserProfile/>
+      <UserProfile />
     </UserContext.Provider>
   );
 }
