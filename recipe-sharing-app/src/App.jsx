ae01cce2bccff0fc3b4a8030c9d0b8476ee7a73d
kaberege <kabgnestor@gmail.com>
2024-08-20 22:09:14 +0200

    added routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import { Route, Router, Routes } from "react-router-dom";
 
 export default function App() {
+  function myRoute() {
+    const path = "Route, Router, Routes";
+    return path;
+  }
+  console.log(myRoute())
   return (
     <div className="app">
       <AddRecipeForm />
