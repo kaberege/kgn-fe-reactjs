5601c2adbb6804337a24919ec71fc2890190e251
kaberege <kabgnestor@gmail.com>
2024-08-20 22:01:19 +0200

    added useNavigate

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import useRecipeStore from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
-
+  const navigate = useNavigate();
+  function navigateHome (){
+    navigate("/");
+  }
   const handleDelete = () => {
     deleteRecipe(recipeId);
   };
 
   return (
-    <button onClick={handleDelete}>Delete Recipe</button>
+    <button onClick={()=> {handleDelete(); navigateHome();}}
+    >
+      Delete Recipe</button>
   );
 };
 
