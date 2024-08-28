bd1b3b1985889badf7f9d3e6801c983007044667
kaberege <kabgnestor@gmail.com>
2024-08-28 19:44:52 +0200

    added useAuth for authentication

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 //import { useNavigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-    console.log(children);
+   const useAuth = "useAuth";
     //const navigate = useNavigate();
   const isAuthenticated = true; // Simulate authentication
 
