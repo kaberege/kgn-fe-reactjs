1a90a198641e39bd9609f5285f9b1bbb9d80d40f
kaberege <kabgnestor@gmail.com>
2024-09-18 11:33:19 +0200

    added full requirements

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -10,13 +10,11 @@ export const fetchUserData = async (username) => {
 const GITHUB_API_URL = 'https://api.github.com/search/users?q=';
 
 export const fetchUserData = async (username, location, minRepos) => {
-    const query = [];
-    if (username) query.push(`${username}`);
-    if (location) query.push(`location:${location}`);
-    if (minRepos) query.push(`repos:>=${minRepos}`);
+  const query = [];
+  if (username) query.push(`${username}`);
+  if (location) query.push(`location:${location}`);
+  if (minRepos) query.push(`repos:>=${minRepos}`);
 
-    const response = await axios.get(`${GITHUB_API_URL}${query.join('+')}`);
-    console.log(response.data)
-    console.log(response.data.items)
-    return response.data.items;
+  const response = await axios.get(`${GITHUB_API_URL}${query.join('+')}`);
+  return response.data.items;
 };
