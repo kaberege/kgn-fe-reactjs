e1005964ae4df9e3a0ddc37ed72e6823e9041ef3
kaberege <kabgnestor@gmail.com>
2025-01-03 16:03:41 +0200

    added new project section

--- kgn-portfolio/src/components/About.jsx
+++ kgn-portfolio/src/components/About.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { FaReact } from "react-icons/fa";
+import { FaReact, FaPython } from "react-icons/fa";
 import { TbBrandJavascript, TbBrandTypescript } from "react-icons/tb";
 
 export default function About() {
+
+    const style = "font-bold text-4xl animate-bounce text-blue-950 dark:text-blue-200";
+
+    const about = [
+        {
+            icon: <FaReact className={style} />,
+            skill: "React"
+        },
+        {
+            icon: <TbBrandJavascript className={style} />,
+            skill: "JavaScript"
+        },
+        {
+            icon: <TbBrandTypescript className={style} />,
+            skill: "TypeScript"
+        },
+        {
+            icon: <FaPython className={style} />,
+            skill: "Django"
+        }
+
+    ];
+
     return (
         <>
             <section id="about">
@@ -10,27 +33,19 @@ export default function About() {
                 <p
                     className='max-w-4xl text-lg mb-10 dark:text-stone-100 transition-colors duration-300 '
                 >
-                    I am a proficient software developer experienced in JavaScript and TypeScript, with a strong focus on the React.js framework. As a fast learner, I work closely with clients to develop efficient, scalable, and user-friendly solutions that tackle real-world challenges. I look forward to collaborating with you to bring your ideas to fruition!
+                    I’m a skilled software developer with expertise in JavaScript, TypeScript, and React.js for building dynamic front-end applications. Additionally, I have strong experience with Python, particularly in backend development using Django and Django REST Framework (DRF), where I focus on creating scalable, secure, and efficient APIs. As a quick learner, I am passionate about collaborating with stakeholders and understanding user needs to craft intuitive solutions that solve real-world problems. I’m excited to bring your ideas to life with efficient and impactful software solutions.
                 </p>
                 <div className='dark:text-stone-100 flex flex-row flex-wrap justify-center w-full sm:max-w-4xl xl:max-w-7xl mx-auto gap-12'>
-                    <div className='flex flex-col items-center justify-center cursor-pointer border-2 border-x-indigo-800 border-y-yellow-400  max-sm:mx-auto bg-blue-400 w-full max-sm:max-w-96 max-sm:h-64 sm:w-64 sm:h-72 rounded-2xl shadow-md hover:scale-105 transition-all duration-300 '>
-                        <FaReact className='font-bold text-4xl animate-bounce text-blue-950 dark:text-blue-200' />
-                        <p className='flex flex-col items-center justify-center font-semibold text-xl sm:text-2xl mt-7'>
-                            <span>React</span> <span>Developer</span>
-                        </p>
-                    </div>
-                    <div className='dark:text-stone-100 flex flex-col items-center justify-center cursor-pointer border-2 border-x-indigo-800 border-y-yellow-400  max-sm:mx-auto bg-blue-400 w-full max-sm:max-w-96 max-sm:h-64 sm:w-64 sm:h-72 rounded-2xl shadow-md hover:scale-105 transition-all duration-300 '>
-                        <TbBrandJavascript className='font-bold text-4xl animate-bounce text-blue-950 dark:text-blue-200' />
-                        <p className='flex flex-col items-center justify-center font-semibold text-xl sm:text-2xl mt-7'>
-                            <span>JavaScript</span> <span>Developer</span>
-                        </p>
-                    </div>
-                    <div className='flex flex-col items-center justify-center cursor-pointer border-2 border-x-indigo-800 border-y-yellow-400   max-sm:mx-auto bg-blue-400 w-full max-sm:max-w-96 max-sm:h-64 sm:w-64 sm:h-72 rounded-2xl shadow-md hover:scale-105 transition-all duration-300 '>
-                        <TbBrandTypescript className='font-bold text-4xl animate-bounce text-blue-950 dark:text-blue-200 transition-colors duration-300' />
-                        <p className='flex flex-col items-center justify-center font-semibold text-xl sm:text-2xl mt-7'
-                        ><span>TypeScript</span> <span>Developer</span>
-                        </p>
-                    </div>
+                    {
+                        about.map((value, index) => (
+                            <div key={index} className='flex flex-col items-center justify-center cursor-pointer border-2 border-x-indigo-800 border-y-yellow-400  max-sm:mx-auto bg-blue-400 w-full max-sm:max-w-96 max-sm:h-64 sm:w-64 sm:h-72 rounded-2xl shadow-md hover:scale-105 transition-all duration-300 '>
+                                {value.icon}
+                                <p className='flex flex-col items-center justify-center font-semibold text-xl sm:text-2xl mt-7'>
+                                    <span>{value.skill}</span> <span>Developer</span>
+                                </p>
+                            </div>
+                        ))
+                    }
                 </div>
             </section>
         </>
---
+++ kgn-portfolio/src/store/projects.js
@@ -0,0 +1,83 @@
+import quiz from "../assets/quiz-project.jpg";
+import tenzy from "../assets/tenzy-project.jpg";
+import vanilla from "../assets/web-project.jpg";
+import social from "../assets/social.jpg";
+
+export const projects = [
+    {
+        link: {
+            href: "https://kaberege-quiz-app.vercel.app/",
+            background: quiz
+        },
+        heading: "Quiz App",
+        description: "Transforming the learning experience with an interactive quiz app that offers personalized assessments and progress tracking. Built on a user-friendly platform using React, JavaScript, and Tailwind CSS, it provides a seamless gateway to engaging and effective learning.",
+        skills: [
+            {
+                class: "text-sm bg-blue-100 dark:bg-blue-800 text-blue-800 dark:text-blue-200 p-1  rounded-full transition-colors duration-300",
+                content: "#React JS"
+            },
+            {
+                class: "text-sm bg-green-100 dark:bg-green-800 text-green-800 dark:text-green-200 p-1  rounded-full transition-colors duration-300",
+                content: "#Tailwind CSS"
+            }
+        ]
+    },
+    {
+        link: {
+            href: "https://kaberege-tenzies-game.vercel.app/",
+            background: tenzy
+        },
+        heading: "Tenzies Game App",
+        description: "Revolutionizing casual gaming with a fun and interactive Tenzies game app. Developed using React and CSS, this user-friendly platform offers an engaging experience while tracking player progress and providing hours of entertainment.",
+        skills: [
+            {
+                class: "text-sm bg-blue-100 dark:bg-blue-800 text-blue-800 dark:text-blue-200 p-1  rounded-full transition-colors duration-300",
+                content: "#React JS"
+            },
+            {
+                class: "text-sm bg-green-100 dark:bg-green-800 text-green-800 dark:text-green-200 p-1  rounded-full transition-colors duration-300",
+                content: "#CSS"
+            }
+        ]
+    },
+    {
+        link: {
+            href: "https://kaberege.github.io/kgn.github.io/",
+            background: vanilla
+        },
+        heading: "Vanilla JS Website",
+        description: "Showcasing the power of simplicity, this website is built using vanilla JavaScript and CSS. It provides an intuitive and responsive user experience, serving as a digital gateway to information and features without the complexity of frameworks.",
+        skills: [
+            {
+                class: "text-sm bg-blue-100 dark:bg-blue-800 text-blue-800 dark:text-blue-200 p-1  rounded-full transition-colors duration-300",
+                content: "#Vanilla JS"
+            },
+            {
+                class: "text-sm bg-green-100 dark:bg-green-800 text-green-800 dark:text-green-200 p-1  rounded-full transition-colors duration-300",
+                content: "#CSS"
+            }
+        ]
+    },
+    {
+        link: {
+            href: "https://kaberege.pythonanywhere.com/",
+            background: social
+        },
+        heading: "Social Media API",
+        description: "A comprehensive API, built with Django and Django REST Framework, simulates the functionalities of a social media platform, including post management, user interactions, notifications, JWT-based authentication...",
+        skills: [
+            {
+                class: "text-sm bg-blue-100 dark:bg-blue-800 text-blue-800 dark:text-blue-200 p-1  rounded-full transition-colors duration-300",
+                content: "#Django"
+            },
+            {
+                class: "text-sm bg-green-100 dark:bg-green-800 text-green-800 dark:text-green-200 p-1  rounded-full transition-colors duration-300",
+                content: "#REST API"
+            },
+            {
+                class: "text-sm bg-yellow-100 dark:bg-yellow-800 text-yellow-800 dark:text-yellow-200 p-1 rounded-full transition-colors duration-300",
+                content: "#JWT Authentication"
+            }
+        ]
+    }
+];
